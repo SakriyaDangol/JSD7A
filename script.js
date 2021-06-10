b7a43778e0ac42742b0321f3caeef4007af3f227
SakriyaDangol <sakriyadangol003@gmail.com>
2021-06-10 18:45:02 +0545

    first commit

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // declared a variable, and also use prompt to take input from user
 var number = prompt('Enter a number');
 
-// this statement will process the input and give the correct output
+// this statement will process the input and give the matching output
 if (number > 0){
     document.write('The number is +ve');
 } else {
